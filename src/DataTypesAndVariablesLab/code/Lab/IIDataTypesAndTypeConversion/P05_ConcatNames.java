e9fdd8b9d9c8f62ac4274f6248be48e07dd39aeb
Alex <aleksandaredochev@gmail.com>
2023-07-06 17:51:00 +0300

    Java execises

--- src/DataTypesAndVariablesLab/code/Lab/IIDataTypesAndTypeConversion/P05_ConcatNames.java
+++ src/DataTypesAndVariablesLab/code/Lab/IIDataTypesAndTypeConversion/P05_ConcatNames.java
@@ -1,4 +1,17 @@
 package DataTypesAndVariablesLab.code.Lab.IIDataTypesAndTypeConversion;
 
+import java.util.Scanner;
+
 public class P05_ConcatNames {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputFirst = scanner.nextLine();
+        String inputSecond = scanner.nextLine();
+        String inputDelimeter = scanner.nextLine();
+
+        String combine = "";
+
+        System.out.printf("%s%s%s",inputFirst,inputDelimeter,inputSecond);
+
+    }
 }
--- src/DataTypesAndVariablesLab/code/Lab/IIDataTypesAndTypeConversion/P06_CharsToString.java
+++ src/DataTypesAndVariablesLab/code/Lab/IIDataTypesAndTypeConversion/P06_CharsToString.java
@@ -1,4 +1,15 @@
 package DataTypesAndVariablesLab.code.Lab.IIDataTypesAndTypeConversion;
 
+import java.util.Scanner;
+
 public class P06_CharsToString {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String inputFirst = scanner.nextLine();
+        String inputSecond = scanner.nextLine();
+        String inputDelimeter = scanner.nextLine();
+
+        System.out.printf("%s%s%s",inputFirst,inputDelimeter,inputSecond);
+    }
 }
