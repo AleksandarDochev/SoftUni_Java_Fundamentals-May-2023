ec8f7da3f3c7d8b4f1a746a7931537cdd20627b8
Alex <aleksandaredochev@gmail.com>
2023-07-06 15:00:35 +0300

    some exercises + docs

--- src/DataTypesAndVariablesLab/code/Lab/P03_ExactSumofRealNumbers.java
+++ src/DataTypesAndVariablesLab/code/Lab/P03_ExactSumofRealNumbers.java
@@ -1,4 +1,19 @@
 package DataTypesAndVariablesLab.code.Lab;
 
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class P03_ExactSumofRealNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int inputNumIterationLength = Integer.parseInt(scanner.nextLine());
+        BigDecimal sum = new BigDecimal(0);
+        for (int i = 0; i < inputNumIterationLength; i++) {
+            BigDecimal inputNumberInIteration = new BigDecimal(scanner.nextLine());
+            sum = sum.add(inputNumberInIteration);
+        }
+        System.out.println(sum);
+
+    }
+
 }
--- src/codePresets.java
+++ src/codePresets.java
@@ -33,8 +33,15 @@ public class codePresets {
             int num = Integer.parseInt(scanner.nextLine());
             sum = sum + num;
         }
-//------------------------------------------------------------------------------------------
-
+//--------------------DIVISIONS----------------------------------
+//        /*System.out.println(10 / 4); // 2 (integral division)
+//        System.out.println(10 / 4.0); // 2.5 (real division)
+//        System.out.println(10 / 0.0); // Infinity
+//        System.out.println(-10 / 0.0); // -Infinity
+//        System.out.println(0 / 0.0); // NaN (not a number)
+//        System.out.println(8 % 2.5); // 0.5 (3 * 2.5 + 0.5 = 8)
+//        System.out.println(10 / 0); // ArithmeticException*/
+//--------------------DIVISIONS----------------------------------
 
 
     }
