fd6041f592870103d7c7dddf88899038c35f25b3
Alex <aleksandaredochev@gmail.com>
2023-07-07 12:25:28 +0300

    Code blueprints for numericals

--- src/DataTypesAndVariablesLab/code/Lab/IIDataTypesAndTypeConversion/P09_CenturiesToMinutes.java
+++ src/DataTypesAndVariablesLab/code/Lab/IIDataTypesAndTypeConversion/P09_CenturiesToMinutes.java
@@ -1,4 +1,26 @@
 package DataTypesAndVariablesLab.code.Lab.IIDataTypesAndTypeConversion;
 
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class P09_CenturiesToMinutes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double centuries = Integer.parseInt(scanner.nextLine());
+        //1 centuries = 100 years = 36524 days = 876581 hours = 52594877 minutes
+        double years = centuries * 100;
+        double days = years * 365.2422;
+        double hours = days * 24;
+        double minutes = hours * 60;
+
+        int intCenturies = (int)centuries;
+        int intYears =(int)years;
+        int intDays = (int)days;
+        int intHours = (int)hours;
+        BigDecimal bigMinutes = new BigDecimal((int)minutes);
+
+
+        System.out.printf("%s centuries = %s years = %s days = %s hours = %s minutes",centuries,intYears,intDays,intDays,bigMinutes);
+
+    }
 }
--- src/numCoversions.java
+++ src/numCoversions.java
@@ -1,3 +1,5 @@
+import java.math.BigDecimal;
+
 public class numCoversions {
     public static void main(String[] args) {
 
@@ -8,6 +10,25 @@ public class numCoversions {
 
         System.out.println((int)numDouble);
          ////////////-NUMBER cast ,casting -----------------
+
+        ////////////////////////////- NUMBER cas double to integer-
+        double years = 100;
+        int intYears =(int)years;
+        System.out.printf("\nDouble : %s " +
+                          "\nInt conversion : %d " +
+                          "\n",years,intYears);
+
+        ////////////////////////////- NUMBER cas double to integer-
+
+        ////////////////-BigDecimal calculation for finances , high precision , very high numbers-----------
+        double bigNumber = 787685.65 * 34325235 *60;
+        BigDecimal extremelyBigNumber = new BigDecimal(bigNumber); //normal
+        BigDecimal extremelyBigNumberCast = new BigDecimal((int)bigNumber); // cast double to int here
+        System.out.printf("\nbigNumber with errors : %s " +
+                "\nNormal big number conversion precise : %s " +
+                "\nCasted big number conversion to int  : %s",bigNumber,extremelyBigNumber,extremelyBigNumberCast);
+        ////////////////-BigDecimal calculation for finances , high precision , very high numbers-----------
+
 
     }
 }
