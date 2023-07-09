2a7f4b631bc88bc3d1d14f05ee61c6de3deb6639
Alex <aleksandaredochev@gmail.com>
2023-07-09 17:32:10 +0300

    Data Types and Variables - Exercise tasks

--- src/DataTypesAndVariables/code/Exercises/P09_SpiceMustFlow.java
+++ src/DataTypesAndVariables/code/Exercises/P09_SpiceMustFlow.java
@@ -1,7 +1,30 @@
 package DataTypesAndVariables.code.Exercises;
 
+import java.util.Scanner;
+
 public class P09_SpiceMustFlow {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int extract = Integer.parseInt(scanner.nextLine());
+
+        int spiceMineDailyYeald = extract;
+        int Days = 0;
+        int minedSpice=0;
+        int spiceMineReservers = 0;
+
+
+        while (spiceMineDailyYeald >= 100){
+            Days++;
+            minedSpice = minedSpice + spiceMineDailyYeald; //Daily extract
+            spiceMineDailyYeald -=10; // decay of mine
+            minedSpice-=26; //worker pay
+
+
+        }
+        minedSpice = minedSpice-26;
+        System.out.println(Days);
+        System.out.println(minedSpice);
 
     }
 }
