21670476cd3621563e3a85054887b18198ab0f7c
Alex <aleksandaredochev@gmail.com>
2023-07-16 17:47:04 +0300

    Course LinksCourse

--- src/Arrays/ExercisesMore/P01_EncryptSortAndPrintArray.java
+++ src/Arrays/ExercisesMore/P01_EncryptSortAndPrintArray.java
@@ -1,7 +1,14 @@
 package Arrays.ExercisesMore;
 
+import java.util.Scanner;
+
 public class P01_EncryptSortAndPrintArray {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int iterationsLoop = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < iterationsLoop; i++) {
+
+        }
 
     }
 }
---
+++ src/LinksCourse.java
@@ -0,0 +1,8 @@
+public class LinksCourse {
+    public static void main(String[] args) {
+        //
+        //       https://softuni.bg/trainings/4095/programming-fundamentals-with-java-may-2023
+
+        // Next  https://softuni.bg/trainings/resources/video/84452/video-31-may-2023-veronika-vaneva-programming-fundamentals-with-java-may-2023/4095
+    }
+}
