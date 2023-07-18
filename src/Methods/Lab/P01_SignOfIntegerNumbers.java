ef2818ed78c394f474610589436abdb51865efbd
Alex <aleksandaredochev@gmail.com>
2023-07-18 15:33:58 +0300

    Methods java lab exercises

--- src/Methods/Lab/P01_SignOfIntegerNumbers.java
+++ src/Methods/Lab/P01_SignOfIntegerNumbers.java
@@ -4,19 +4,23 @@ import java.util.Scanner;
 
 public class P01_SignOfIntegerNumbers {
     public static void main(String[] args) {
-
-        printHello();
-
+        Scanner scanner = new Scanner(System.in);
+        int num = Integer.parseInt(scanner.nextLine());
+        printNumberSign(num);
 
     }
-
-    //public is to use it everywhere
-    //static idk
-    //void is when it does something but it doesn't return any value , like "walk 5 meters" we just do something no result
-    //is no void is set then "take a sh*t" , we are expecting a value or result  that is the sh*t
-    public static void printHello(){
-        System.out.println("Hello");
-        //https://softuni.bg/trainings/resources/video/84452/video-31-may-2023-veronika-vaneva-programming-fundamentals-with-java-may-2023/4095
-        //-2:25:22
+    public static void printNumberSign(int num){
+        if (num < 0){
+            System.out.printf("The number %d is negative.\n",num);
+        } else if (num>0) {
+            System.out.printf("The number %d is positive.\n",num);
+        } else if (num == 0) {
+            System.out.printf("The number %d is zero.\n",num);
+        }
     }
+
+
+
+
+
 }
--- src/Methods/Lab/P02_Grades.java
+++ src/Methods/Lab/P02_Grades.java
@@ -1,4 +1,32 @@
 package Methods.Lab;
 
+import java.util.Scanner;
+
 public class P02_Grades {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double num = Double.parseDouble(scanner.nextLine());
+        printGrade(num);
+
+    }
+
+    public static void printGrade(double num){
+//         2.00 – 2.99 - "Fail"
+//•        3.00 – 3.49 - "Poor"
+//•        3.50 – 4.49 - "Good"
+//•        4.50 – 5.49 - "Very good"
+//•        5.50 – 6.00 - "Excellent"
+        if (num >= 2.00 && num<=2.99){
+            System.out.println("Fail");
+        } else if (num >=3.00  && num<=3.49) {
+            System.out.println("Poor");
+        }else if (num >=3.50  && num<=4.49) {
+            System.out.println("Good");
+        }else if (num >=4.50  && num<=5.49) {
+            System.out.println("Very good");
+        }else if (num >=5.50  && num<=6) {
+            System.out.println("Excellent");
+        }
+
+    }
 }
