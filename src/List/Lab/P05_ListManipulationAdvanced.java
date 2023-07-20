c28b0717270014da53302cd4d79caa4112a24c5a
Alex <aleksandaredochev@gmail.com>
2023-07-21 01:31:17 +0300

    List exercise  and presets end for day

--- src/List/Lab/P05_ListManipulationAdvanced.java
+++ src/List/Lab/P05_ListManipulationAdvanced.java
@@ -1,4 +1,10 @@
 package List.Lab;
 
 public class P05_ListManipulationAdvanced {
+    public static void main(String[] args) {
+
+
+        //https://softuni.bg/trainings/resources/video/84732/video-07-june-2023-veronika-vaneva-programming-fundamentals-with-java-may-2023/4095
+        //Time : -27:18
+    }
 }
