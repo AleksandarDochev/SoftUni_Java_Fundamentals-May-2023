59051dda5fab0254576d4ee9bb4c8c4273bdd067
Alex <aleksandaredochev@gmail.com>
2023-07-20 22:23:06 +0300

    List exercise 1 and presets

--- src/List/Lab/P01_SumAdjacentEqualNumbers.java
+++ src/List/Lab/P01_SumAdjacentEqualNumbers.java
@@ -1,4 +1,16 @@
 package List.Lab;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class P01_SumAdjacentEqualNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int inputNums = Integer.parseInt(scanner.nextLine());
+
+        List<Integer> numsList = new ArrayList<>();
+        numsList.add(inputNums);
+    }
+
 }
