4be3714f6bd0f2d2b0172185f2be50675e2b6de2
Alex <aleksandaredochev@gmail.com>
2023-07-25 17:12:46 +0300

    List exercises

--- src/ListsPresets.java
+++ src/ListsPresets.java
@@ -1,8 +1,5 @@
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -74,28 +71,26 @@ public class ListsPresets {
         //4 5 6 7 89 34 2 -> 4, 5, 6, 7, 89, 34, 2
         //--------ArrayList join function add changes to all elements
 
-        //===============ArrayList print contents foreach with delimeter
-        String input = scanner.nextLine();
-        List<Integer> numbersListInteger767 = Arrays
-                .stream(input.split(" "))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-        /////
-        for (int item: numbersListInteger767) {
-            System.out.println(item+" ");
-        }
-        /////
-        //===============ArrayList print contents foreach with delimeter
-
-        //-----------ArrayList print out clean no [] no ,-------
+        //-----------ArrayList print content out clean no [] no ,with delimiter and etc -------
         List<Integer> numList5765 = new ArrayList<>(Arrays.asList(5,5,6,78,876));
         List<String> nameList676 = new ArrayList<>(Arrays.asList("Victor","4iki","Gobler"));
         System.out.println(String.join(" ",nameList676));
-        System.out.println(numList5765.toString().replaceAll("[\\[\\],]", ""));
+        Collections.sort(nameList676); //sorts by Alphabetical name the list
+        System.out.println(String.join(" ",nameList676));//prints out sorted
         System.out.println(); //[5, 5, 6, 78, 876]
-        System.out.println(numList5765.toString().replaceAll("[\\[\\],]",""));
-        //[5, 5, 6, 78, 876] -> 5 5 6 78 876
-        //-----------ArrayList print out clean no [] no ,-------
+        System.out.println(numList5765.toString().replaceAll("[\\[\\],]",""));//[5, 5, 6, 78, 876] -> 5 5 6 78 876
+        for (int item: numList5765) {
+            System.out.println(item+" ");
+        }//[5, 5, 6, 78, 876] -> 5 5 6 78 876
+        ///// print sorted and with numbers
+        int lineNumber=1;
+        for (String name:nameList676) {
+            System.out.printf("%d.%s%n",lineNumber++,name);
+        }
+        /////
+
+        //-----------ArrayList print content out clean no [] no ,with delimiter and etc -------
+
 
 
     }
--- src/test.java
+++ src/test.java
@@ -1,15 +1,27 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class test {
     public static void main(String[] args) {
 
-
-        List<String> nameList676 = new ArrayList<>(Arrays.asList(
-                "Victor", "4iki", "Gobler", "Miner"));
+        List<Integer> numList5765 = new ArrayList<>(Arrays.asList(5,5,6,78,876));
+        List<String> nameList676 = new ArrayList<>(Arrays.asList("Victor","4iki","Gobler"));
         System.out.println(String.join(" ",nameList676));
-
+        Collections.sort(nameList676); //sorts by Alphabetical name the list
+        System.out.println(String.join(" ",nameList676));//prints out sorted
+        System.out.println(); //[5, 5, 6, 78, 876]
+        System.out.println(numList5765.toString().replaceAll("[\\[\\],]",""));//[5, 5, 6, 78, 876] -> 5 5 6 78 876
+        for (int item: numList5765) {
+            System.out.println(item+" ");
+        }//[5, 5, 6, 78, 876] -> 5 5 6 78 876
+        ///// print sorted and with numbers
+        int lineNumber=1;
+        for (String name:nameList676) {
+            System.out.printf("%d.%s%n",lineNumber++,name);
+        }
+        /////
     }
 }
