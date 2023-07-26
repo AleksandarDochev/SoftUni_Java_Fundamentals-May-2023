657352a7d2f6a105295306bb34fdf178d9775d6e
Alex <aleksandaredochev@gmail.com>
2023-07-26 20:07:32 +0300

    Object and Classes lab exercises and presets

--- src/Java08_ObjectsAndClasses/Lab/P05_Students.java
+++ src/Java08_ObjectsAndClasses/Lab/P05_Students.java
@@ -1,4 +1,8 @@
 package Java08_ObjectsAndClasses.Lab;
 
 public class P05_Students {
+    public static void main(String[] args) {
+        //https://softuni.bg/trainings/resources/video/85693/video-22-june-2023-veronika-vaneva-programming-fundamentals-with-java-may-2023/4095
+        // -43:44
+    }
 }
